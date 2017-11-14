4f0ec3fc6bc28556a99a86bf0b7288bf4cf6afac
meganokeefe <megan037@gmail.com>
2017-11-14 15:35:44 -0500

    Matcher queries for storm

--- playstorm.go
+++ playstorm.go
@@ -2,8 +2,10 @@ package main // Wrapper on bolt
 
 import (
 	"log"
+	"os"
 
 	"github.com/asdine/storm"
+	"github.com/asdine/storm/q"
 )
 
 type Vacation struct {
@@ -16,6 +18,55 @@ type Vacation struct {
 }
 
 func main() {
+	complexQuery()
+	os.Remove("my.db")
+}
+
+func complexQuery() {
+	db, err := storm.Open("my.db")
+	if err != nil {
+		log.Fatalf("Could not open: %v", err)
+	}
+	defer db.Close()
+	v := Vacation{
+		ID:            123,
+		Name:          "EuropeAdventure",
+		Cost:          314.15,
+		Kind:          "backpacking",
+		International: true,
+		PackingList:   []string{"backpack", "boots", "compass", "map", "train pass"},
+	}
+
+	// INSERT
+	err = db.Save(&v)
+	if err != nil {
+		log.Fatalf("Could not save EuropeAdventure: %v", err)
+	}
+
+	v = Vacation{
+		ID:            124,
+		Name:          "USAAdventure",
+		Cost:          314.15,
+		Kind:          "backpacking",
+		International: false,
+		PackingList:   []string{"backpack", "boots", "compass", "map", "train pass"},
+	}
+
+	// INSERT
+	err = db.Save(&v)
+	if err != nil {
+		log.Fatalf("Could not save EuropeAdventure: %v", err)
+	}
+
+	var result []Vacation
+	err = db.Select(q.Eq("Kind", "backpacking"), q.Eq("International", true)).Find(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(result)
+}
+
+func easyStuff() {
 	db, err := storm.Open("my.db")
 	if err != nil {
 		log.Fatalf("Could not open: %v", err)
